0e04bef005e38b2c53afd73f6b8d2520bed26333
Synaptic ApS <61604189+synapticlab@users.noreply.github.com>
2020-08-02 23:39:24 +0430

    Update Wave of life.R

--- R_codes/CAIR_comparisons/Wave of Life/Wave of life.R
+++ R_codes/CAIR_comparisons/Wave of Life/Wave of life.R
@@ -30,8 +30,8 @@ all_organisms <- read_excel(temp, sheet = "All organisms")
 from <- 0.875
 to <- 0.945
 wave_life_dens <- density(all_organisms$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2_a.svg', pointsize = pointsize, width = width*2, height = height)
-plot(wave_life_dens, main = "", col="#E64B35",
+svglite(file = 'Fig2A.svg', pointsize = pointsize, width = width*2, height = height)
+plot(wave_life_dens, main = "", col="#cf202d",
      xlab='CAIR of all organisms', xaxt='n',
      ylab='', yaxt='n', family=family)
 axis(side=1, at=seq(from = from, to = to, by = 0.005))
@@ -44,8 +44,8 @@ proteobacteria <- read_excel(temp, sheet = "Proteobacteria")
 from <- 0.890
 to <- 0.936
 proteo_dens <- density(proteobacteria$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2_b.svg', pointsize = pointsize, width = width, height = height)
-plot(proteo_dens, main = "", col="#3C5488",
+svglite(file = 'Fig2B.svg', pointsize = pointsize, width = width, height = height)
+plot(proteo_dens, main = "", col="#faa41a",
      xlab='CAIR of the Proteobacteria phylum',
      ylab='', yaxt ='n', family=family)
 title(ylab="CAIR Density", line=0.5, cex.lab=1, family=family)
@@ -57,8 +57,8 @@ gammaproteobacteria <- read_excel(temp, sheet = "Gammaproteobacteria")
 from <- 0.900
 to <- 0.936
 gammaproteo_dens <- density(gammaproteobacteria$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2_c.svg', pointsize = pointsize, width = width, height = height)
-plot(gammaproteo_dens, main = "", col="#00A087",
+svglite(file = 'Fig2C.svg', pointsize = pointsize, width = width, height = height)
+plot(gammaproteo_dens, main = "", col="#0b7791",
      xlab='CAIR of the Gammaproteobacteria class',
      ylab='', yaxt ='n', family=family)
 title(ylab="CAIR Density", line=0.5, cex.lab=1, family=family)
@@ -70,8 +70,8 @@ enterobacterales <- read_excel(temp, sheet = "Enterobacterales")
 from <- 0.910
 to <- 0.936
 entero_dens <- density(enterobacterales$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2_d.svg', pointsize = pointsize, width = width, height = height)
-plot(entero_dens, main = "", col="#AE1F63",
+svglite(file = 'Fig2D.svg', pointsize = pointsize, width = width, height = height)
+plot(entero_dens, main = "", col="#4b5437",
      xlab='CAIR of the Enterobacterales order',
      ylab='', yaxt ='n', family=family)
 title(ylab="CAIR Density", line=0.5, cex.lab=1, family=family)
@@ -83,8 +83,8 @@ enterobacteriaceae <- read_excel(temp, sheet = "Enterobacteriaceae")
 from <- 0.926
 to <- 0.935
 enterobacter_dens <- density(enterobacteriaceae$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2_e.svg', pointsize = pointsize, width = width, height = height)
-plot(enterobacter_dens, main = "", col="#CC9900",
+svglite(file = 'Fig2E.svg', pointsize = pointsize, width = width, height = height)
+plot(enterobacter_dens, main = "", col="#9b4822",
      xlab='CAIR of the Enterobacteriaceae family',
      ylab='', yaxt ='n', family=family)
 title(ylab="CAIR Density", line=0.5, cex.lab=1, family=family)
@@ -96,8 +96,8 @@ escherichia <- read_excel(temp, sheet = "Escherichia")
 from <- 0.930
 to <- 0.935
 escherichia_dens <- density(escherichia$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2_f.svg', pointsize = pointsize, width = width, height = height)
-plot(escherichia_dens, main = "", col="#7E6148",
+svglite(file = 'Fig2F.svg', pointsize = pointsize, width = width, height = height)
+plot(escherichia_dens, main = "", col="#6c5877",
      xlab='CAIR of the Escherichia genus',
      ylab='', yaxt ='n', family=family)
 title(ylab="CAIR Density", line=0.5, cex.lab=1, family=family)
@@ -109,8 +109,8 @@ e_coli <- read_excel(temp, sheet = "E. coli")
 from <- 0.931
 to <- 0.935
 e_coli_dens <- density(e_coli$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2_g.svg', pointsize = pointsize, width = width, height = height)
-plot(e_coli_dens, main = "", col="#1A237E",
+svglite(file = 'Fig2G.svg', pointsize = pointsize, width = width, height = height)
+plot(e_coli_dens, main = "", col="#52c0a3",
      xlab='CAIR of the E. coli organism',
      ylab='', yaxt ='n', family=family)
 title(ylab="CAIR Density", line=0.5, cex.lab=1, family=family)
