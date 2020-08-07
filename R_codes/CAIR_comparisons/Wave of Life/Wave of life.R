a774b73fca96c00a8ffa39c07c90a096b2765334
Synaptic ApS <61604189+synapticlab@users.noreply.github.com>
2020-08-07 12:18:17 +0430

    Update Wave of life.R

--- R_codes/CAIR_comparisons/Wave of Life/Wave of life.R
+++ R_codes/CAIR_comparisons/Wave of Life/Wave of life.R
@@ -30,7 +30,7 @@ all_organisms <- read_excel(temp, sheet = "All organisms")
 from <- 0.875
 to <- 0.945
 wave_life_dens <- density(all_organisms$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2A.svg', pointsize = pointsize, width = width*2, height = height)
+svglite(file = 'Fig3A.svg', pointsize = pointsize, width = width*2, height = height)
 plot(wave_life_dens, main = "", col="#cf202d",
      xlab='CAIR of all organisms', xaxt='n',
      ylab='', yaxt='n', family=family)
@@ -44,7 +44,7 @@ proteobacteria <- read_excel(temp, sheet = "Proteobacteria")
 from <- 0.890
 to <- 0.936
 proteo_dens <- density(proteobacteria$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2B.svg', pointsize = pointsize, width = width, height = height)
+svglite(file = 'Fig3B.svg', pointsize = pointsize, width = width, height = height)
 plot(proteo_dens, main = "", col="#faa41a",
      xlab='CAIR of the Proteobacteria phylum',
      ylab='', yaxt ='n', family=family)
@@ -57,7 +57,7 @@ gammaproteobacteria <- read_excel(temp, sheet = "Gammaproteobacteria")
 from <- 0.900
 to <- 0.936
 gammaproteo_dens <- density(gammaproteobacteria$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2C.svg', pointsize = pointsize, width = width, height = height)
+svglite(file = 'Fig3C.svg', pointsize = pointsize, width = width, height = height)
 plot(gammaproteo_dens, main = "", col="#0b7791",
      xlab='CAIR of the Gammaproteobacteria class',
      ylab='', yaxt ='n', family=family)
@@ -70,7 +70,7 @@ enterobacterales <- read_excel(temp, sheet = "Enterobacterales")
 from <- 0.910
 to <- 0.936
 entero_dens <- density(enterobacterales$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2D.svg', pointsize = pointsize, width = width, height = height)
+svglite(file = 'Fig3D.svg', pointsize = pointsize, width = width, height = height)
 plot(entero_dens, main = "", col="#4b5437",
      xlab='CAIR of the Enterobacterales order',
      ylab='', yaxt ='n', family=family)
@@ -83,7 +83,7 @@ enterobacteriaceae <- read_excel(temp, sheet = "Enterobacteriaceae")
 from <- 0.926
 to <- 0.935
 enterobacter_dens <- density(enterobacteriaceae$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2E.svg', pointsize = pointsize, width = width, height = height)
+svglite(file = 'Fig3E.svg', pointsize = pointsize, width = width, height = height)
 plot(enterobacter_dens, main = "", col="#9b4822",
      xlab='CAIR of the Enterobacteriaceae family',
      ylab='', yaxt ='n', family=family)
@@ -96,7 +96,7 @@ escherichia <- read_excel(temp, sheet = "Escherichia")
 from <- 0.930
 to <- 0.935
 escherichia_dens <- density(escherichia$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2F.svg', pointsize = pointsize, width = width, height = height)
+svglite(file = 'Fig3F.svg', pointsize = pointsize, width = width, height = height)
 plot(escherichia_dens, main = "", col="#6c5877",
      xlab='CAIR of the Escherichia genus',
      ylab='', yaxt ='n', family=family)
@@ -109,7 +109,7 @@ e_coli <- read_excel(temp, sheet = "E. coli")
 from <- 0.931
 to <- 0.935
 e_coli_dens <- density(e_coli$CAIR, from=from, to=to, bw=bw)
-svglite(file = 'Fig2G.svg', pointsize = pointsize, width = width, height = height)
+svglite(file = 'Fig3G.svg', pointsize = pointsize, width = width, height = height)
 plot(e_coli_dens, main = "", col="#52c0a3",
      xlab='CAIR of the E. coli organism',
      ylab='', yaxt ='n', family=family)
